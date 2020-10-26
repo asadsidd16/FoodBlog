93e9d2eb5e036d07191682e0ebe5f06300fc2575
asad <asad@hotmail.com>
2020-10-26 13:29:14 -0700

    php database conditions for pulling user data

---
+++ app/database/db.php
@@ -0,0 +1,45 @@
+<?php 
+
+require('connect.php');
+
+
+
+function dd($value){ //for testing will delete
+    echo "<pre>", print_r($value, true), "</pre>";
+    die();
+}
+
+function selectAll($table, $conditions = []){
+    global $conn;
+    $sql = "SELECT * FROM $table";
+    if(empty($conditions)){
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $records;
+    } else {
+        //return records that match conditions
+        //$sql = "SELECT * FROM $table WHERE username='asad' AND admin=1";
+       
+        $i = 0;
+        foreach ($conditions as $key => $value){
+            if($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            } else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+        dd($sql);
+
+    }
+
+}
+
+$conditions = [
+    'admin' => 1,
+    'username' => 'asad'
+];
+
+$users = selectAll('users', $conditions);
+dd($users);
